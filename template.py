7dcccb72d83535c5c0c045fd729a1460a0505281
ARYAN SINGH <thisisaryan1ia@gmail.com>
2025-01-08 12:34:16 +0530

    added

--- template.py
+++ template.py
@@ -11,26 +11,27 @@ list_of_files = [
     "src/prompt.py",
     ".env",
     "setup.py",
-    "app.py",
     "research/trials.ipynb",
-   " test.py"
+    "app.py",
+    "store_index.py",
+    "static/.gitkeep",
+    "templates/chat.html"
+
 ]
 
 
 for filepath in list_of_files:
-    filepath = Path(filepath)
-    filedir, filename = os.path.split(filepath)
+   filepath = Path(filepath)
+   filedir, filename = os.path.split(filepath)
 
+   if filedir !="":
+      os.makedirs(filedir, exist_ok=True)
+      logging.info(f"Creating directory; {filedir} for the file {filename}")
 
-    if filedir !="":
-        os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+   if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+      with open(filepath, 'w') as f:
+         pass
+         logging.info(f"Creating empty file: {filepath}")
 
-    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
-        with open(filepath, "w") as f:
-            pass
-            logging.info(f"Creating empty file: {filepath}")
-
-
-    else:
-        logging.info(f"{filename} is already exists")
+   else:
+      logging.info(f"{filename} is already created")
